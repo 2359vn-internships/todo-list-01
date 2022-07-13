7fe2dffcb04c0318e2ad352d05519367d528e5bc
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2022-07-13 13:26:53 +0700

    feat: button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,9 +1,36 @@
 import { Component } from "react";
-import styles from "./Button.css";
+import styles from "./Button.module.css";
 
 class Button extends Component {
 	render() {
-		return;
+		const { children, href, target, variant, ...props } = this.props;
+
+		if (href) {
+			return (
+				<a
+					href={href}
+					target={target || "_self"}
+					role="button"
+					className={`${styles.btn} ${
+						"delete" === variant ? styles.deleteBtn : ""
+					}`}
+					{...props}
+				>
+					{children}
+				</a>
+			);
+		}
+
+		return (
+			<button
+				className={`${styles.btn} ${
+					"delete" === variant ? styles.deleteBtn : ""
+				}`}
+				{...props}
+			>
+				{children}
+			</button>
+		);
 	}
 }
 export default Button;
--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -1,19 +1,23 @@
 import { Component } from "react";
 import Input from "../../components/input/Input";
+import Button from "../../components/button/Button";
 import styles from "./Details.module.css";
 
 class Details extends Component {
 	render() {
 		return (
 			<div className={styles.details}>
-				<div className={styles["page-title"]}>
+				<div className={styles.title}>
 					<h1 className="h1">Details</h1>
 				</div>
-				<div className={styles["details-body"]}>
-					<form>
+				<div className={styles.detailsBody}>
+					<form className={styles.form}>
 						<Input isMultipleLines={false} />
 						<Input isMultipleLines={true} />
-						<div className={styles["container-btns"]}></div>
+						<div className={styles.btnsContainer}>
+							<Button>Save</Button>
+							<Button variant="delete">Delete</Button>
+						</div>
 					</form>
 				</div>
 			</div>
