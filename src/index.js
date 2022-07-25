36b04b87d6376e122a8c4711d62ff237a8f45f67
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2022-07-25 16:33:07 +0700

    chore: install react-i18next, i18next, setup languages json file

--- src/index.js
+++ src/index.js
@@ -4,8 +4,25 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import common_vi from "./translations/vi/common.json";
+import common_en from "./translations/en/common.json";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+i18next.use(initReactI18next).init({
+	interpolation: { escapeValue: false },
+	lng: "vi",
+	resources: {
+		en: {
+			common: common_en,
+		},
+		vi: {
+			common: common_vi,
+		},
+	},
+});
 
 root.render(
 	<React.StrictMode>
